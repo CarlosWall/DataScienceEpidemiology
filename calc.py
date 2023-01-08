5a69bb92e0028fced906e7e941ead22a9d8bb8ae
CarlosWall <74607776+CarlosWall@users.noreply.github.com>
2023-01-08 02:16:09 +0000

    calc

---
+++ calc.py
@@ -0,0 +1,8 @@
+n1 = input("Digite o primeiro número: ")
+n2 = input("Digite o segundo número: ")
+
+print('A soma é: ', float(n1)+float(n2))
+print('A divisão é: ', float(n1)/float(n2))
+print('A subtração é: ', float(n1)-float(n2))
+print('A multiplicação é: ', float(n1)*float(n2))
+print('A potência é: ', float(n1)**float(n2))
